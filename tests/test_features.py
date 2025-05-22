14673c679e745b70f219ea9bfa5761dd46b41f4e
yunline <yunline@foxmail.com>
2025-05-22 22:05:28 +0800

    Add tests for menu popup position, align and allow_right_click

--- tests/test_features.py
+++ tests/test_features.py
@@ -533,4 +533,68 @@ def test_submenu_circular_reference():
     with pytest.raises(ValueError):
         Menu1.insert_item(0, sub2)
 
-        
+def test_popup_position():
+    class MyMenu(pywintray.Menu):
+        item1 = pywintray.MenuItem.string("item1")
+        item2 = pywintray.MenuItem.string("item2")
+        item3 = pywintray.MenuItem.string("item3")
+    
+    with popup_in_new_thread(MyMenu, position=(300, 400)):
+        menu = get_current_menu()
+        rect = menu.rectangle()
+        assert rect.left==300
+        assert rect.top==400
+
+def test_popup_align():
+    class MyMenu(pywintray.Menu):
+        item1 = pywintray.MenuItem.string("item1")
+        item2 = pywintray.MenuItem.string("item2")
+        item3 = pywintray.MenuItem.string("item3")
+    
+    with popup_in_new_thread(MyMenu, position=(300, 400), horizontal_align="center"):
+        menu = get_current_menu()
+        rect = menu.rectangle()
+        assert (rect.left+rect.right)//2==300
+    
+    with popup_in_new_thread(MyMenu, position=(300, 400), horizontal_align="right"):
+        menu = get_current_menu()
+        rect = menu.rectangle()
+        assert rect.right==300
+    
+    with popup_in_new_thread(MyMenu, position=(300, 400), vertical_align="center"):
+        menu = get_current_menu()
+        rect = menu.rectangle()
+        assert (rect.top+rect.bottom)//2==400
+    
+    with popup_in_new_thread(MyMenu, position=(300, 400), vertical_align="bottom"):
+        menu = get_current_menu()
+        rect = menu.rectangle()
+        assert rect.bottom==400
+
+def test_popup_allow_right_click():
+    from pywinauto.mouse import right_click
+
+    CB_CALLED = None
+    def cb(_):
+        nonlocal CB_CALLED
+        CB_CALLED = "called"
+    class MyMenu(pywintray.Menu):
+        item1 = pywintray.MenuItem.string("item1", callback=cb)
+    
+    with popup_in_new_thread(MyMenu, allow_right_click=False):
+        menu = get_current_menu()
+        rect = menu.menu_item("item1").rectangle()
+        x = (rect.left + rect.right) // 2
+        y = (rect.top + rect.bottom) // 2
+        right_click(coords=(x, y))
+    
+    assert CB_CALLED is None
+
+    with popup_in_new_thread(MyMenu, allow_right_click=True):
+        menu = get_current_menu()
+        rect = menu.menu_item("item1").rectangle()
+        x = (rect.left + rect.right) // 2
+        y = (rect.top + rect.bottom) // 2
+        right_click(coords=(x, y))
+    
+    assert CB_CALLED=="called"
